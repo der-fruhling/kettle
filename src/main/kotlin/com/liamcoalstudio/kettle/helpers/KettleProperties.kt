63b5c35f138c5c047f82bd8fc59de18c4eb47aef
Liam Cole <liam@liamiam.com>
2021-01-28 01:46:51 -0800

    Add default configuration

--- src/main/kotlin/com/liamcoalstudio/kettle/helpers/KettleProperties.kt
+++ src/main/kotlin/com/liamcoalstudio/kettle/helpers/KettleProperties.kt
@@ -1,8 +1,11 @@
 package com.liamcoalstudio.kettle.helpers
 
+import com.google.common.io.Files
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.stream.JsonReader
+import org.intellij.lang.annotations.Language
+import java.io.File
 import java.io.FileReader
 import java.io.FileWriter
 import kotlin.properties.Delegates
@@ -14,8 +17,28 @@ object KettleProperties {
     lateinit var spawn: ConfiguredSpawnLocation
     var flat by Delegates.notNull<Boolean>()
 
+    @Language("json")
+    private val defaultConfig = """
+        {
+          "motd": "Kettle Server!",
+          "viewDistance": 4,
+          "gamemode": 1,
+          "flat": true,
+          "spawn": {
+            "x": 0.0,
+            "y": 4.0,
+            "z": 0.0
+          }
+        }
+    """.trimIndent()
+
     fun load() {
         val gson = Gson()
+        if(!File("properties.json").exists()) {
+            val wr = FileWriter("properties.json")
+            wr.write(defaultConfig)
+            wr.close()
+        }
         val p = gson.fromJson(FileReader("properties.json"), GsonProperties::class.java)
         motd = p.motd
         viewDistance = p.viewDistance
